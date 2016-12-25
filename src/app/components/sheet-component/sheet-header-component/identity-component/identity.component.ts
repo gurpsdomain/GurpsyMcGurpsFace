ed979b127d1fe8c734b8e14f56aae406868b980c
Meron Brouwer <meron.brouwer@gmail.com>
2016-12-25 20:53:35 +0100

    Small style improvements. Next step will be introducing angular-flex

--- src/app/components/sheet-component/sheet-header-component/identity-component/identity.component.ts
+++ src/app/components/sheet-component/sheet-header-component/identity-component/identity.component.ts
@@ -3,7 +3,8 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-identity-component',
     templateUrl: 'identity.component.html',
-    styleUrls: ['../../sheet.component.css']
+    styleUrls: ['../../sheet.component.css',
+        'identity.component.css']
 })
 export class IdentityComponent {
 }
