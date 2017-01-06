0ebbacbafd5200d091bd90ae9feaa2d94e787bca
Meron Brouwer <meron.brouwer@gmail.com>
2017-01-06 13:23:02 +0100

    Added a new entry into the menu for clearing local data

--- src/app/gurpsy.component.ts
+++ src/app/gurpsy.component.ts
@@ -4,38 +4,38 @@ import {LanguagesService} from './services/languages-service/languages.service';
 import {ThemesService} from './services/themes-service/themes.service';
 
 @Component({
-    selector: 'gurpsy-root',
-    templateUrl: './gurpsy.component.html',
-    styleUrls: ['./gurpsy.component.css'],
-    providers: [LanguagesService, ThemesService]
+  selector: 'gurpsy-root',
+  templateUrl: './gurpsy.component.html',
+  styleUrls: ['./gurpsy.component.css'],
+  providers: [LanguagesService, ThemesService]
 })
 export class GurpsyComponent implements OnInit {
 
-    private translateService: TranslateService;
-    private languagesService: LanguagesService;
-    private themesService: ThemesService;
+  private translateService: TranslateService;
+  private languagesService: LanguagesService;
+  private themesService: ThemesService;
 
-    constructor(translate: TranslateService, languages: LanguagesService, themes: ThemesService) {
-        this.translateService = translate;
-        this.languagesService = languages;
-        this.themesService = themes;
-    }
+  constructor(translate: TranslateService, languages: LanguagesService, themes: ThemesService) {
+    this.translateService = translate;
+    this.languagesService = languages;
+    this.themesService = themes;
+  }
 
-    ngOnInit(): void {
-        this.initLanguages();
-        this.initThemes();
-    }
+  ngOnInit(): void {
+    this.initLanguages();
+    this.initThemes();
+  }
 
-    initLanguages(): void {
-        this.languagesService.getAvailableLanguagesLocales().then(
-            availableLanguages => this.translateService.addLangs(availableLanguages));
-        this.languagesService.getDefaultLocale().then(
-            defaultLocale => this.translateService.setDefaultLang(defaultLocale));
-        this.languagesService.getCurrentLocale().then(
-            currentlocale => this.translateService.use(currentlocale));
-    }
+  initLanguages(): void {
+    this.languagesService.getAvailableLanguagesLocales().then(
+      availableLanguages => this.translateService.addLangs(availableLanguages));
+    this.languagesService.getDefaultLocale().then(
+      defaultLocale => this.translateService.setDefaultLang(defaultLocale));
+    this.languagesService.getCurrentLocale().then(
+      currentlocale => this.translateService.use(currentlocale));
+  }
 
-    initThemes(): void {
-        this.themesService.initializeThemes();
-    }
+  initThemes(): void {
+    this.themesService.initializeThemes();
+  }
 }
