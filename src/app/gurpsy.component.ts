888b407814d8fdb5b7f2a2d858e356c28083d9f2
Meron Brouwer <meron.brouwer@gmail.com>
2017-01-13 12:57:32 +0100

    Fixed the dialogs and removed the reference to themes

--- src/app/gurpsy.component.ts
+++ src/app/gurpsy.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from 'ng2-translate';
 import {LanguagesService} from './services/languages-service/languages.service';
+import {MdDialog, MdDialogRef} from '@angular/material';
+import {DeleteSettingsDialogComponent} from './components/dialog-component/delete-settings-dialog/delete-settings-dialog.component';
+import {OpenSheetDialogComponent} from './components/dialog-component/open-sheet-dialog/open-sheet-dialog.component';
 
 @Component({
   selector: 'gurpsy-root',
@@ -12,11 +15,17 @@ export class GurpsyComponent implements OnInit {
 
   private translateService: TranslateService;
   private languagesService: LanguagesService;
+
+  private openSheetDialogRef: MdDialogRef<OpenSheetDialogComponent>;
+  private deleteSettingsDialogRef: MdDialogRef<DeleteSettingsDialogComponent>;
+
+  public dialog: MdDialog;
   public isDarkTheme: boolean = false;
 
-  constructor(translate: TranslateService, languages: LanguagesService) {
+  constructor(translate: TranslateService, languages: LanguagesService, dialog: MdDialog) {
     this.translateService = translate;
     this.languagesService = languages;
+    this.dialog = dialog;
   }
 
   ngOnInit(): void {
@@ -30,5 +39,25 @@ export class GurpsyComponent implements OnInit {
       defaultLocale => this.translateService.setDefaultLang(defaultLocale));
     this.languagesService.getCurrentLocale().then(
       currentlocale => this.translateService.use(currentlocale));
+  }
+
+  onOpenSheetDialog(): void {
+    this.openSheetDialogRef = this.dialog.open(OpenSheetDialogComponent, {
+      disableClose: false
+    });
+
+    this.openSheetDialogRef.afterClosed().subscribe(
+      this.openSheetDialogRef = null
+    );
+  }
+
+  onOpenDeleteSettingsDialog(): void {
+    this.deleteSettingsDialogRef = this.dialog.open(DeleteSettingsDialogComponent, {
+      disableClose: false
+    });
+
+    this.deleteSettingsDialogRef.afterClosed().subscribe(
+      this.deleteSettingsDialogRef = null
+    );
   }
 }
