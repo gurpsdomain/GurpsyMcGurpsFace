dfe4171befc2297c96d44110b4c8f434a615452c
Meron Brouwer <meron.brouwer@gmail.com>
2017-01-18 15:01:33 +0100

    Removed unused model object

--- src/app/models/theme.ts
+++
@@ -1,9 +0,0 @@
-export class Theme {
-    name: string;
-    stylesheet: string;
-
-    constructor(name: string, stylesheet: string) {
-        this.name = name;
-        this.stylesheet = stylesheet;
-    }
-}
