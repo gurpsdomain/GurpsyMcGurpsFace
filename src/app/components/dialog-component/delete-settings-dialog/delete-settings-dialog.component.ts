afe14f208d74515b15bb3e38752e3144aca6d634
Meron Brouwer <meron.brouwer@gmail.com>
2017-02-13 16:44:42 +0100

    Some work in progress regarding the delete dialog and stored sheets

--- src/app/components/dialog-component/delete-settings-dialog/delete-settings-dialog.component.ts
+++ src/app/components/dialog-component/delete-settings-dialog/delete-settings-dialog.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {MdDialogRef} from '@angular/material';
 import {StorageService} from '../../../services/storage-service/storage.service';
+import {SheetMapEntry} from '../../../model/json/sheetmap';
 
 @Component({
   selector: 'gurpsy-delete-settings-dialog',
@@ -9,6 +10,8 @@ import {StorageService} from '../../../services/storage-service/storage.service'
 })
 export class DeleteSettingsDialogComponent {
 
+  public storedSheets: SheetMapEntry[] = [];
+
   private dialogRef: MdDialogRef<DeleteSettingsDialogComponent>;
   private storageService: StorageService;
 
@@ -16,10 +19,17 @@ export class DeleteSettingsDialogComponent {
   constructor(dialogRef: MdDialogRef<DeleteSettingsDialogComponent>, storage: StorageService) {
     this.dialogRef = dialogRef;
     this.storageService = storage;
+
+    this.initPreviouslyOpenedSheetList();
   }
 
   onDeleteSettings(): void {
     this.storageService.clearStorage();
     this.dialogRef.close();
+  }
+
+  private initPreviouslyOpenedSheetList(): void {
+    this.storageService.getAllStoredSheets().then(
+      sheetMap => this.storedSheets = sheetMap.sheets);
   }
 }
