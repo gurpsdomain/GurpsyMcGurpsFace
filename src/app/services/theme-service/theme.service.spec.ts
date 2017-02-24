8941a086e0cfa9be3c4f7fe00cae08a0ad1268d6
Meron Brouwer <meron.brouwer@gmail.com>
2017-02-24 13:07:11 +0100

    Added required dependencies

--- src/app/services/theme-service/theme.service.spec.ts
+++ src/app/services/theme-service/theme.service.spec.ts
@@ -1,15 +1,22 @@
 /* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
-import { ThemeService } from './theme.service';
+import {TestBed} from '@angular/core/testing';
+import {ThemeService} from './theme.service';
 import {StorageService} from '../storage-service/storage.service';
+import {ThemeStorageDelegate} from '../storage-service/delegates/theme-storage-delegate/theme-storage-delegate';
+import {LanguageStorageDelegate} from '../storage-service/delegates/language-storage-delegate/language-storage-delegate';
+import {SheetStorageDelegate} from '../storage-service/delegates/sheet-storage-delegate/sheet-storage-delegate';
+import {JsonService} from '../json-service/json.service';
 
 describe('ThemeService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         ThemeService,
-        StorageService
+        StorageService,
+        ThemeStorageDelegate,
+        LanguageStorageDelegate,
+        SheetStorageDelegate,
+        JsonService
       ]
     });
   });
