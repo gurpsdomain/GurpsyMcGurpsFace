6dd4bfcb5f6ebdf4a84d40c6c16e6ed7c4196d49
Meron Brouwer <meron.brouwer@gmail.com>
2017-03-14 12:10:35 +0100

    First draft of the Skills component

--- src/app/components/sheet-component/reusable-components/skills-component/skills.component.spec.ts
+++ src/app/components/sheet-component/reusable-components/skills-component/skills.component.spec.ts
@@ -2,6 +2,12 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {SkillsComponent} from './skills.component';
 import {TranslateModule} from 'ng2-translate';
+import {ModelReadService} from '../../../../services/model-read-service/model-read.service';
+import {JsonService} from '../../../../services/json-service/json.service';
+import {StorageService} from '../../../../services/storage-service/storage.service';
+import {ThemeStorageDelegate} from '../../../../services/storage-service/delegates/theme-storage-delegate/theme-storage-delegate';
+import {LanguageStorageDelegate} from '../../../../services/storage-service/delegates/language-storage-delegate/language-storage-delegate';
+import {SheetStorageDelegate} from '../../../../services/storage-service/delegates/sheet-storage-delegate/sheet-storage-delegate';
 
 describe('SkillsComponent', () => {
   let component: SkillsComponent;
@@ -14,6 +20,14 @@ describe('SkillsComponent', () => {
       ],
       imports: [
         TranslateModule.forRoot()
+      ],
+      providers: [
+        ModelReadService,
+        JsonService,
+        StorageService,
+        ThemeStorageDelegate,
+        LanguageStorageDelegate,
+        SheetStorageDelegate
       ]
     })
       .compileComponents();
--- src/app/components/sheet-component/reusable-components/skills-component/skills.component.ts
+++ src/app/components/sheet-component/reusable-components/skills-component/skills.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {ModelReadService} from '../../../../services/model-read-service/model-read.service';
+import {Sheet} from '../../../../model/sheet';
 
 @Component({
   selector: 'gurpsy-skills-component',
   templateUrl: 'skills.component.html',
-  styleUrls: ['../../sheet.component.scss']
+  styleUrls: [
+    './skills.component.scss',
+    '../../sheet.component.scss'
+  ]
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+  public sheet: Sheet;
+  private modelReadService: ModelReadService;
 
-  ngOnInit() {
+  constructor(modelReadService: ModelReadService) {
+    this.modelReadService = modelReadService;
+  }
+
+  ngOnInit(): void {
+    this.sheet = this.modelReadService.getSheet();
+    this.modelReadService.modelChange$.subscribe(sheet => this.sheet = sheet);
   }
 }
