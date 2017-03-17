8bd645ad8d48f2e8890d8dbc01a8cc90c73ec263
Meron Brouwer <meron.brouwer@gmail.com>
2017-03-17 09:48:42 +0100

    Added implementation for the advantages and disadvantages component

--- src/app/components/sheet-component/reusable-components/advantages-component/advantages.component.ts
+++ src/app/components/sheet-component/reusable-components/advantages-component/advantages.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ModelReadService} from '../../../../services/model-read-service/model-read.service';
+import {Sheet} from '../../../../model/sheet';
 
 @Component({
   selector: 'gurpsy-advantages-component',
   templateUrl: 'advantages.component.html',
-  styleUrls: ['../../sheet.component.scss']
+  styleUrls: ['./advantages.component.scss',
+    '../../sheet.component.scss'
+  ]
 })
 export class AdvantagesComponent implements OnInit {
 
-  constructor() { }
+  public sheet: Sheet;
+  private modelReadService: ModelReadService;
 
-  ngOnInit() {
+  constructor(modelReadService: ModelReadService) {
+    this.modelReadService = modelReadService;
   }
 
+  ngOnInit(): void {
+    this.sheet = this.modelReadService.getSheet();
+    this.modelReadService.modelChange$.subscribe(sheet => this.sheet = sheet);
+  }
 }
