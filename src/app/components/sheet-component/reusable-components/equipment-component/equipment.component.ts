4fb59f4f9b52af2dcd290c9c7a16324c704d76c1
Meron Brouwer <meron.brouwer@gmail.com>
2017-03-19 14:41:30 +0100

    Implemented the equipment component

--- src/app/components/sheet-component/reusable-components/equipment-component/equipment.component.ts
+++ src/app/components/sheet-component/reusable-components/equipment-component/equipment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Sheet} from '../../../../model/sheet';
+import {ModelReadService} from '../../../../services/model-read-service/model-read.service';
 
 @Component({
   selector: 'gurpsy-equipment-component',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
-  constructor() { }
+  public sheet: Sheet;
+  private modelReadService: ModelReadService;
 
-  ngOnInit() {
+  constructor(modelReadService: ModelReadService) {
+    this.modelReadService = modelReadService;
   }
 
+  ngOnInit(): void {
+    this.sheet = this.modelReadService.getSheet();
+    this.modelReadService.modelChange$.subscribe(sheet => this.sheet = sheet);
+  }
 }
