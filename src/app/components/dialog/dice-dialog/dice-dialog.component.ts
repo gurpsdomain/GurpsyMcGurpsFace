c4e4d2688df7a8ee251552102ccf39dd6e0f4e30
Meron Brouwer <meron.brouwer@gmail.com>
2017-03-31 13:48:51 +0200

    Second attempt to get 3D to work. Hooray, it works

--- src/app/components/dialog/dice-dialog/dice-dialog.component.ts
+++ src/app/components/dialog/dice-dialog/dice-dialog.component.ts
@@ -1,49 +1,125 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ViewChild, AfterViewInit, Input, ElementRef} from '@angular/core';
 import * as THREE from 'three';
-import Scene = THREE.Scene;
-import PerspectiveCamera = THREE.PerspectiveCamera;
-
 
 @Component({
   selector: 'gurpsy-delete-settings-dialog',
   templateUrl: './dice-dialog.component.html',
   styleUrls: ['./dice-dialog.component.scss']
 })
-export class DiceDialogComponent implements OnInit {
-
-  private scene: Scene;
-  private camera: PerspectiveCamera;
+export class DiceDialogComponent implements AfterViewInit {
 
   @ViewChild('diceView') diceView;
 
-  ngOnInit(): void {
-    this.createWorld();
+  private camera: THREE.PerspectiveCamera;
+
+  @ViewChild('canvas')
+  private canvasRef: ElementRef;
+
+  private cube: THREE.Mesh;
+
+  private renderer: THREE.WebGLRenderer;
+
+  private scene: THREE.Scene;
+
+  @Input()
+  public rotationSpeedX = 0.005;
+
+  @Input()
+  public rotationSpeedY = 0.01;
+
+  @Input()
+  public size = 200;
+
+  @Input()
+  public texture = '/assets/textures/crate.gif';
+
+  @Input()
+  public cameraZ = 400;
+
+  @Input()
+  public fieldOfView = 70;
+
+  @Input()
+  public nearClippingPane = 1;
+
+  @Input()
+  public farClippingPane = 1000;
+
+  /**
+   * Update scene after resizing.
+   */
+  public onResize() {
+    this.camera.aspect = this.getAspectRatio();
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
   }
 
-  private createWorld(): void {
+  /**
+   * We need to wait until template is bound to DOM, as we need the view
+   * dimensions to create the scene. We could create the cube in a Init hook,
+   * but we would be unable to add it to the scene until now.
+   */
+  public ngAfterViewInit() {
+    this.createScene();
+    this.createCube();
+    this.startRenderingLoop();
+  }
+
+  public onRethrow(): void {
+    console.log('Rethrowing');
+  }
+
+
+  private get canvas(): HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
+
+  private animateCube() {
+    this.cube.rotation.x += this.rotationSpeedX;
+    this.cube.rotation.y += this.rotationSpeedY;
+  }
+
+  private createCube() {
+    const texture = new THREE.TextureLoader().load(this.texture);
+    const material = new THREE.MeshBasicMaterial({map: texture});
+
+    const geometry = new THREE.BoxBufferGeometry(this.size, this.size, this.size);
+    this.cube = new THREE.Mesh(geometry, material);
+
+    // Add cube to scene
+    this.scene.add(this.cube);
+  }
+
+  private createScene() {
+    /* Scene */
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, this.diceView.innerWidth / this.diceView.innerHeight, 0.1, 1000);
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(this.diceView.innerWidth, this.diceView.innerHeight);
-    this.diceView.appendChild(renderer.domElement);
+    /* Camera */
+    const aspectRatio = this.getAspectRatio();
+    this.camera = new THREE.PerspectiveCamera(
+      this.fieldOfView,
+      aspectRatio,
+      this.nearClippingPane,
+      this.farClippingPane
+    );
+    this.camera.position.z = this.cameraZ;
+  }
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    const cube = new THREE.Mesh(geometry, material);
-    this.scene.add(cube);
+  private getAspectRatio() {
+    return this.canvas.clientWidth / this.canvas.clientHeight;
+  }
 
-    this.camera.position.z = 5;
+  private startRenderingLoop() {
+    this.renderer = new THREE.WebGLRenderer({canvas: this.canvas});
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
 
-    const render = function () {
+    const component: DiceDialogComponent = this;
+    (function render() {
       requestAnimationFrame(render);
-
-      cube.rotation.x += 0.1;
-      cube.rotation.y += 0.1;
-
-      renderer.render(this.scene, this.camera);
-    };
-
-    render();
+      component.animateCube();
+      component.renderer.render(component.scene, component.camera);
+    }());
   }
 }
