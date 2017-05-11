0a4380a50d6afddd06901ce0c83f4e4bfcf49558
Meron Brouwer <meron.brouwer@gmail.com>
2017-05-11 13:45:25 +0200

    Trying out some thing regarding the base_url and en.json loading

--- src/app/gurpsy.module.ts
+++ src/app/gurpsy.module.ts
@@ -56,7 +56,7 @@ import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(http: Http) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
