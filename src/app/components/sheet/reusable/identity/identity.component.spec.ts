1b4cbe07dc77ec128464d5927bfca645483d45f4
Meron Brouwer <meron.brouwer@gmail.com>
2017-07-29 20:41:29 +0200

    First unittest for components

--- src/app/components/sheet/reusable/identity/identity.component.spec.ts
+++ src/app/components/sheet/reusable/identity/identity.component.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 import {IdentityComponent} from './identity.component';
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {TranslateModule} from '@ngx-translate/core';
 import {ModelService} from '../../../../services/front-end/model/model.service';
 import {StorageService} from '../../../../services/back-end/storage/storage.service';
@@ -10,10 +10,25 @@ import {SheetStorageDelegate} from '../../../../services/back-end/storage/delega
 import {SettingsService} from '../../../../services/front-end/settings/settings.service';
 import {LoggingService} from '../../../../services/back-end/logging/logging.service';
 import {GurpsyMaterialModule} from '../../../../modules/material.module';
+import {By} from '@angular/platform-browser';
+import {Template} from '../../../../models/sheet/template/template.model';
+import {Sheet} from '../../../../models/sheet/model/sheet.model';
 
 describe('IdentityComponent', function () {
   let component: IdentityComponent;
   let fixture: ComponentFixture<IdentityComponent>;
+
+  let modelService: ModelService;
+
+  let template: Template;
+  let sheet: Sheet;
+
+  const CHARACTER_NAME = 'Dai Blackthorn';
+  const CHARACTER_NAME_SELECTOR = '.name';
+  const CHARACTER_TITLE = 'Captain';
+  const CHARACTER_TITLE_SELECTOR = '.title';
+  const CHARACTER_RELIGION = 'Thaoist';
+  const CHARACTER_RELIGION_SELECTOR = '.religion';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -39,9 +54,66 @@ describe('IdentityComponent', function () {
   beforeEach(() => {
     fixture = TestBed.createComponent(IdentityComponent);
     component = fixture.componentInstance;
+
+    modelService = fixture.debugElement.injector.get(ModelService);
+
+    template = new Template();
+    sheet = new Sheet(template);
+    sheet.metaData.identity.name = CHARACTER_NAME;
+    sheet.metaData.identity.title = CHARACTER_TITLE;
+    sheet.metaData.identity.religion = CHARACTER_RELIGION;
+
+    spyOn(modelService, 'getModel')
+      .and.returnValue(Promise.resolve(sheet));
+    spyOn(modelService, 'getTemplate')
+      .and.returnValue(Promise.resolve(template));
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show the correct name from the sheet model', () => {
+    const de = fixture.debugElement.query(By.css(CHARACTER_NAME_SELECTOR));
+    const el = de.nativeElement;
+
+    component.model = sheet;
+
+    fixture.detectChanges();
+    expect(el.textContent.trim()).toBe(CHARACTER_NAME);
+  });
+
+  it('should show the correct title from the sheet model', () => {
+    const de = fixture.debugElement.query(By.css(CHARACTER_TITLE_SELECTOR));
+    const el = de.nativeElement;
+
+    component.model = sheet;
+
+    fixture.detectChanges();
+    expect(el.textContent.trim()).toBe(CHARACTER_TITLE);
+  });
+
+  it('should show the correct religion from the sheet model', () => {
+    const de = fixture.debugElement.query(By.css(CHARACTER_RELIGION_SELECTOR));
+    const el = de.nativeElement;
+
+    component.model = sheet;
+
+    fixture.detectChanges();
+    expect(el.textContent.trim()).toBe(CHARACTER_RELIGION);
+  });
+
+  it('should have a template set after component is initialized', fakeAsync(() => {
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+    expect(component.template).toBe(template);
+  }));
+
+  it('should have a model set after component is initialised', fakeAsync(() => {
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+    expect(component.model).toBe(sheet);
+  }));
 });
--- src/app/components/sheet/reusable/player-information/player-information.component.spec.ts
+++ src/app/components/sheet/reusable/player-information/player-information.component.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 import {PlayerInformationComponent} from './player-information.component';
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {TranslateModule} from '@ngx-translate/core';
 import {ModelService} from '../../../../services/front-end/model/model.service';
 import {StorageService} from '../../../../services/back-end/storage/storage.service';
@@ -10,11 +10,26 @@ import {SheetStorageDelegate} from '../../../../services/back-end/storage/delega
 import {SettingsService} from '../../../../services/front-end/settings/settings.service';
 import {LoggingService} from '../../../../services/back-end/logging/logging.service';
 import {GurpsyMaterialModule} from '../../../../modules/material.module';
+import {Sheet} from '../../../../models/sheet/model/sheet.model';
+import {Template} from '../../../../models/sheet/template/template.model';
+import {By} from '@angular/platform-browser';
 
 ////////  SPECS  /////////////
 describe('PlayerInformationComponent', function () {
   let component: PlayerInformationComponent;
   let fixture: ComponentFixture<PlayerInformationComponent>;
+
+  let modelService: ModelService;
+
+  let template: Template;
+  let sheet: Sheet;
+
+  const PLAYER = 'Daan van Berkel';
+  const PLAYER_SELECTOR = '.player';
+  const CAMPAIGN = 'Paul\'s total party kill';
+  const CAMPAIGN_SELECTOR = '.campaign';
+  const CREATED_ON = '01-01-2001';
+  const CREATED_ON_SELECTOR = '.createdOn';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -40,9 +55,66 @@ describe('PlayerInformationComponent', function () {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlayerInformationComponent);
     component = fixture.componentInstance;
+
+    modelService = fixture.debugElement.injector.get(ModelService);
+
+    template = new Template();
+    sheet = new Sheet(template);
+    sheet.metaData.playerInformation.player = PLAYER;
+    sheet.metaData.playerInformation.campaign = CAMPAIGN;
+    sheet.metaData.playerInformation.createdOn = CREATED_ON;
+
+    spyOn(modelService, 'getModel')
+      .and.returnValue(Promise.resolve(sheet));
+    spyOn(modelService, 'getTemplate')
+      .and.returnValue(Promise.resolve(template));
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show the correct player from the sheet model', () => {
+    const de = fixture.debugElement.query(By.css(PLAYER_SELECTOR));
+    const el = de.nativeElement;
+
+    component.model = sheet;
+
+    fixture.detectChanges();
+    expect(el.textContent.trim()).toBe(PLAYER);
+  });
+
+  it('should show the correct campaign from the sheet model', () => {
+    const de = fixture.debugElement.query(By.css(CAMPAIGN_SELECTOR));
+    const el = de.nativeElement;
+
+    component.model = sheet;
+
+    fixture.detectChanges();
+    expect(el.textContent.trim()).toBe(CAMPAIGN);
+  });
+
+  it('should show the correct createdOn from the sheet model', () => {
+    const de = fixture.debugElement.query(By.css(CREATED_ON_SELECTOR));
+    const el = de.nativeElement;
+
+    component.model = sheet;
+
+    fixture.detectChanges();
+    expect(el.textContent.trim()).toBe(CREATED_ON);
+  });
+
+  it('should have a template set after component is initialized', fakeAsync(() => {
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+    expect(component.template).toBe(template);
+  }));
+
+  it('should have a model set after component is initialized', fakeAsync(() => {
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+    expect(component.model).toBe(sheet);
+  }));
 });
