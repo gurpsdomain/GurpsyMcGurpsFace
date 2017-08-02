694f4defe61c7b6dcef5c0b38981ffb9ac475782
Meron Brouwer <meron.brouwer@gmail.com>
2017-08-02 21:00:25 +0200

    Implemented a new test, related to #43

--- src/app/services/front-end/sheet/sheet.service.spec.ts
+++ src/app/services/front-end/sheet/sheet.service.spec.ts
@@ -7,9 +7,15 @@ import {SettingsService} from '../settings/settings.service';
 import {TranslateModule} from '@ngx-translate/core';
 import {LoggingService} from '../../back-end/logging/logging.service';
 import {SheetService} from './sheet.service';
+import {Template} from '../../../models/sheet/template/template.model';
 
 describe('SheetService', () => {
+
+  let storageService: StorageService;
+
   beforeEach(() => {
+
+
     TestBed.configureTestingModule({
       imports: [
         TranslateModule.forRoot()
@@ -23,9 +29,29 @@ describe('SheetService', () => {
         StorageService
       ]
     });
+
+    storageService = TestBed.get(StorageService);
   });
 
   it('should be created', inject([SheetService], (service: SheetService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should store a template with the current date when updateTemplate() is called', inject([SheetService], (service: SheetService) => {
+    spyOn(storageService, 'storeTemplate');
+
+    const today = new Date();
+    const yesterday = new Date();
+    yesterday.setDate(today.getDate() - 1);
+
+    const yesterdaysTemplate = new Template();
+    yesterdaysTemplate.lastModified = yesterday;
+
+    const todaysTemplate = new Template();
+    todaysTemplate.lastModified = today;
+
+    service.updateTemplate(yesterdaysTemplate);
+
+    expect(storageService.storeTemplate).toHaveBeenCalledWith(todaysTemplate);
   }));
 });
