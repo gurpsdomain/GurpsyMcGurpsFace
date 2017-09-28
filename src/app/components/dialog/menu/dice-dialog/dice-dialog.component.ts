1828a27ed72b30ea5c5a5a9b7752da50e85330a7
Meron Brouwer <meron.brouwer@gmail.com>
2017-09-28 09:22:18 +0200

    [ehnancement] Work in progress from integrating Whitestorm.js

--- src/app/components/dialog/menu/dice-dialog/dice-dialog.component.ts
+++ src/app/components/dialog/menu/dice-dialog/dice-dialog.component.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import * as WHS from 'whs';
 
 @Component({
   templateUrl: 'dice-dialog.component.html',
@@ -6,15 +7,23 @@ import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 })
 export class DiceDialogComponent implements AfterViewInit {
 
+  private app: WHS.App;
+
   @ViewChild('canvas')
   private canvasRef: ElementRef;
 
   public ngAfterViewInit() {
+    const app = new WHS.App([
+      new WHS.ElementModule(this.getCanvas()),
+      new WHS.SceneModule()
+    ]);
 
   }
 
   public onRethrow(): void {
     console.log('Rethrowing');
+
+    this.app.start();
   }
 
   private getCanvas(): HTMLCanvasElement {
