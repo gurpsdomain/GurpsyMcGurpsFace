d2e2297faa69d40ef11ebdbc772252220cdfc8a4
Meron Brouwer <meron.brouwer@gmail.com>
2017-10-27 10:02:00 +0200

    [enhancement] Added unittests for weight

--- src/app/presentation/components/generic/weight-display/weight-display.component.spec.ts
+++ src/app/presentation/components/generic/weight-display/weight-display.component.spec.ts
@@ -1,14 +1,20 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import {WeightDisplayComponent} from './weight-display.component';
 import {TranslateModule} from '@ngx-translate/core';
 import {LoggingService} from '../../../../services/logging/logging.service';
 import {TemplateRepository} from '../../../../repositories/template/template.repository';
 import {SheetService} from '../../../../services/sheet/sheet.service';
+import {Sheet} from '../../../../models/sheet/sheet.model';
+import {SheetTemplate} from '../../../../models/sheet-template/sheet-template.model';
 
 describe('WeightDisplayComponent', () => {
   let component: WeightDisplayComponent;
   let fixture: ComponentFixture<WeightDisplayComponent>;
+
+  let sheetService: SheetService;
+  let sheet: Sheet;
+  let template: SheetTemplate;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -30,10 +36,33 @@ describe('WeightDisplayComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WeightDisplayComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    sheetService = TestBed.get(SheetService);
+
+    template = new SheetTemplate();
+    sheet = new Sheet(template);
+
+    spyOn(sheetService, 'getSheet')
+      .and.returnValue(Promise.resolve(sheet));
+    spyOn(sheetService, 'getTemplate')
+      .and.returnValue(Promise.resolve(template));
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
-  });
+  })
+
+  it('should have a sheet set after component is initialized', fakeAsync(() => {
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+    expect(component.sheet).toBe(sheet);
+  }));
+
+  it('should have a template set after component is initialized', fakeAsync(() => {
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+    expect(component.template).toBe(template);
+  }));
 });
