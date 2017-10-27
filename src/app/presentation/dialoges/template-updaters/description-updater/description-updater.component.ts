1cfa79f77fed4450579735a0b6b6129ad34bb261
Meron Brouwer <meron.brouwer@gmail.com>
2017-10-27 09:33:55 +0200

    [enhancement] Added units to the description-updater

--- src/app/presentation/dialoges/template-updaters/description-updater/description-updater.component.ts
+++ src/app/presentation/dialoges/template-updaters/description-updater/description-updater.component.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
 import {TemplateUpdaterDialogComponent} from '../template-updater-dialog.component';
+import {Unit} from '../../../../models/sheet-template/metadata/enums/unit';
 
 @Component({
   templateUrl: './description-updater.component.html',
   styleUrls: ['./description-updater.component.scss']
 })
 export class DescriptionUpdaterComponent extends TemplateUpdaterDialogComponent {
+
+  public unit = Unit;
 }
