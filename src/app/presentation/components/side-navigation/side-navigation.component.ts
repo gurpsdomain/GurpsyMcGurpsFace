a6795b59521973c45d9cc6652dd10b16b9a93181
Meron Brouwer <meron.brouwer@gmail.com>
2017-12-18 11:55:49 +0100

    [refactor] Cleaned up the sidenav

--- src/app/presentation/components/side-navigation/side-navigation.component.ts
+++ src/app/presentation/components/side-navigation/side-navigation.component.ts
@@ -1,6 +1,5 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {SheetViewingComponent} from '../../sheet-viewing.component';
-import {SheetService} from '../../../services/sheet/sheet.service';
 
 @Component({
   selector: 'gurpsy-side-navigation',
@@ -9,15 +8,7 @@ import {SheetService} from '../../../services/sheet/sheet.service';
 })
 export class SideNavigationComponent extends SheetViewingComponent implements OnInit {
 
-  @Output() onShowLibrary: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Output() onCloseSideNavigation: EventEmitter<any> = new EventEmitter();
-
-  public showLibrary = false;
-  public showLibraryMenu = false;
-
-  constructor(modelService: SheetService) {
-    super(modelService);
-  }
 
   ngOnInit(): void {
     super.ngOnInit();
@@ -25,16 +16,6 @@ export class SideNavigationComponent extends SheetViewingComponent implements On
 
   public onCloseSideNav(): void {
     this.onCloseSideNavigation.next();
-  }
-
-  public onLibraryClick(): void {
-    this.onShowLibrary.next(this.toggleShowLibrary());
-  }
-
-
-  private toggleShowLibrary(): boolean {
-    this.showLibrary = !this.showLibrary;
-    return this.showLibrary;
   }
 }
 
