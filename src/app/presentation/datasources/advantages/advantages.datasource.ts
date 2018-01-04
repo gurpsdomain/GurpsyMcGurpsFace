fbe5dd0a3a964af37d9ede83ced2b61c9de0b68c
Meron Brouwer <meron.brouwer@gmail.com>
2018-01-04 09:49:18 +0100

    [enhancement] Added filtering on the advantages table

--- src/app/presentation/datasources/advantages/advantages.datasource.ts
+++ src/app/presentation/datasources/advantages/advantages.datasource.ts
@@ -6,11 +6,21 @@ import 'rxjs/add/observable/merge';
 import {AdvantagesLibrary} from '../../../models/library/advantages/advantages.model';
 import {AdvantageLibrary} from '../../../models/library/advantage/advantage.model';
 import {MatPaginator, MatSort} from '@angular/material';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Subject} from 'rxjs/Subject';
 
 
 export class AdvantagesDatasource extends DataSource<any> {
 
-  constructor(private _advantages: AdvantagesLibrary, private _paginator: MatPaginator, private _sort: MatSort) {
+  _filterChange = new BehaviorSubject('');
+
+  private dataSetSizeSource = new Subject<number>();
+  public dataSetSizeSource$ = this.dataSetSizeSource.asObservable();
+
+
+  constructor(private _advantages: AdvantagesLibrary,
+              private _paginator: MatPaginator,
+              private _sort: MatSort) {
     super();
   }
 
@@ -19,18 +29,36 @@ export class AdvantagesDatasource extends DataSource<any> {
     const displayDataChanges = [
       this._paginator.page,
       this._sort.sortChange,
+      this._filterChange
     ];
 
     return Observable.merge(...displayDataChanges).map(() => {
       const data = this._advantages.advantages.slice();
 
-      return this.getSlicedData(this.getSortedData(data));
+      return this.getSlicedData(this.getSortedData(this.getFilteredData(data)));
     });
   }
 
   disconnect() {
   }
 
+  /**
+   * Get the filterstring.
+   *
+   * @return {string}
+   */
+  get filter(): string {
+    return this._filterChange.value;
+  }
+
+  /**
+   * Set the filter string on this datasource.
+   *
+   * @param {string} filter
+   */
+  set filter(filter: string) {
+    this._filterChange.next(filter);
+  }
 
   private getSlicedData(data: AdvantageLibrary[]): AdvantageLibrary[] {
     const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
@@ -62,4 +90,15 @@ export class AdvantagesDatasource extends DataSource<any> {
     });
   }
 
+  private getFilteredData(data: AdvantageLibrary[]): AdvantageLibrary[] {
+
+    const filteredDataSet = data.slice().filter((advantage: AdvantageLibrary) => {
+      const searchStr = (advantage.name).toLowerCase();
+      return searchStr.indexOf(this.filter.toLowerCase()) !== -1;
+    });
+
+    this.dataSetSizeSource.next(filteredDataSet.length);
+
+    return filteredDataSet;
+  }
 }
--- src/app/presentation/template/update-advantages/update-advantages.component.ts
+++ src/app/presentation/template/update-advantages/update-advantages.component.ts
@@ -1,9 +1,13 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {LibraryService} from '../../../services/library/library.service';
 import {AdvantagesLibrary} from '../../../models/library/advantages/advantages.model';
 import {AdvantageLibrary} from '../../../models/library/advantage/advantage.model';
 import {AdvantagesDatasource} from '../../datasources/advantages/advantages.datasource';
 import {MatPaginator, MatSort} from '@angular/material';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'gurpsy-update-advantages',
@@ -13,6 +17,7 @@ import {MatPaginator, MatSort} from '@angular/material';
 export class UpdateAdvantagesComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('filter') filter: ElementRef;
 
   displayedColumns = ['name', 'type', 'reference'];
   dataSource = undefined;
@@ -23,15 +28,29 @@ export class UpdateAdvantagesComponent implements OnInit {
 
   ngOnInit(): void {
     this.libraryService.getAdvantages().then(advantages => this.setAdvantages(advantages));
+    Observable.fromEvent(this.filter.nativeElement, 'keyup')
+      .debounceTime(150)
+      .distinctUntilChanged()
+      .subscribe(() => {
+        if (!this.dataSource) {
+          return;
+        }
+        this.dataSource.filter = this.filter.nativeElement.value;
+      });
   }
 
   private setAdvantages(advantages: AdvantagesLibrary): void {
     this.dataSize = advantages.advantages.length;
     this.dataSource = new AdvantagesDatasource(advantages, this.paginator, this.sort);
+    this.dataSource.dataSetSizeSource$.subscribe(size => this.setDataSetSize(size));
   }
 
   public onSelectAdvantage(advantage: AdvantageLibrary): void {
     console.log('AdvantageTemplate selected: ', advantage);
+  }
+
+  private setDataSetSize(size: number): void {
+    this.dataSize = size;
   }
 }
 
