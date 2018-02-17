d62bee39578b2353d3b6357b084f65b16450f8f7
Meron Brouwer <meron.brouwer@gmail.com>
2018-02-17 14:32:07 +0100

    [enhancement] Refactored the Babylon.js related code and added two extra dice and a dice tray

---
+++ src/app/presentation/dialoges/generic/dice-dialog/scenes/dice-tray.factory.ts
@@ -0,0 +1,48 @@
+export class DiceTrayFactory {
+
+  public static createDiceTray(scene: BABYLON.Scene): void {
+
+    const groundMat = new BABYLON.StandardMaterial('groundMat', scene);
+    groundMat.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+    groundMat.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+    groundMat.diffuseTexture = new BABYLON.Texture('/assets/textures/walnut-tabletop.jpg', scene);
+    groundMat.backFaceCulling = false;
+
+    const border0 = BABYLON.Mesh.CreateBox('border0', 1, scene);
+    border0.scaling = new BABYLON.Vector3(1, 10, 40);
+    border0.position.y = -5.0;
+    border0.position.x = -20.0;
+    border0.checkCollisions = true;
+    border0.receiveShadows = true;
+    border0.material = groundMat;
+
+    const border1 = BABYLON.Mesh.CreateBox('border1', 1, scene);
+    border1.scaling = new BABYLON.Vector3(1, 10, 40);
+    border1.position.y = -5.0;
+    border1.position.x = 20.0;
+    border1.checkCollisions = true;
+    border1.receiveShadows = true;
+    border1.material = groundMat;
+
+    const border2 = BABYLON.Mesh.CreateBox('border2', 1, scene);
+    border2.scaling = new BABYLON.Vector3(40, 10, 1);
+    border2.position.y = -5.0;
+    border2.position.z = 20.0;
+    border2.checkCollisions = true;
+    border2.receiveShadows = true;
+    border2.material = groundMat;
+
+    const border3 = BABYLON.Mesh.CreateBox('border3', 1, scene);
+    border3.scaling = new BABYLON.Vector3(40, 10, 1);
+    border3.position.y = -5.0;
+    border3.position.z = -20.0;
+    border3.checkCollisions = true;
+    border3.receiveShadows = true;
+    border3.material = groundMat;
+
+    border0.physicsImpostor = new BABYLON.PhysicsImpostor(border0, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, scene);
+    border1.physicsImpostor = new BABYLON.PhysicsImpostor(border1, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, scene);
+    border2.physicsImpostor = new BABYLON.PhysicsImpostor(border2, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, scene);
+    border3.physicsImpostor = new BABYLON.PhysicsImpostor(border3, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, scene);
+  }
+}
--- src/app/presentation/dialoges/generic/dice-dialog/scenes/physics.ts
+++ src/app/presentation/dialoges/generic/dice-dialog/scenes/physics.ts
@@ -1,4 +1,6 @@
 import {Engine, HemisphericLight, Light, MeshBuilder, Scene, TargetCamera, Vector3} from 'babylonjs';
+import {DieFactory} from './die.factory';
+import {DiceTrayFactory} from './dice-tray.factory';
 
 export class Physics {
 
@@ -14,25 +16,14 @@ export class Physics {
   public createScene(): void {
     this._scene = new BABYLON.Scene(this._engine);
 
-    const camera = new BABYLON.FreeCamera('Camera', new BABYLON.Vector3(0, 0, -20), this._scene);
+    const camera = new BABYLON.FreeCamera('Camera', new BABYLON.Vector3(0, 4, -30), this._scene);
     camera.attachControl(this._canvas, true);
     camera.checkCollisions = true;
     camera.applyGravity = true;
     camera.setTarget(new BABYLON.Vector3(0, 0, 0));
 
     const light = new BABYLON.DirectionalLight('dir02', new BABYLON.Vector3(0.2, -1, 0), this._scene);
-    light.position = new BABYLON.Vector3(0, 80, 0);
-
-    // Material
-    const materialAmiga = new BABYLON.StandardMaterial('amiga', this._scene);
-    materialAmiga.diffuseTexture = new BABYLON.Texture('/assets/textures/walnut-tabletop.jpg', this._scene);
-    materialAmiga.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-    // materialAmiga.diffuseTexture.uScale = 5;
-    // materialAmiga.diffuseTexture.vScale = 5;
-
-    const materialAmiga2 = new BABYLON.StandardMaterial('amiga', this._scene);
-    materialAmiga2.diffuseTexture = new BABYLON.Texture('/assets/textures/walnut-tabletop.jpg', this._scene);
-    materialAmiga2.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+    light.position = new BABYLON.Vector3(0, 40, 0);
 
     // Shadows
     const shadowGenerator = new BABYLON.ShadowGenerator(2048, light);
@@ -41,18 +32,11 @@ export class Physics {
     this._scene.enablePhysics(null, new BABYLON.OimoJSPlugin());
 
     // Dice
-    const box0 = BABYLON.Mesh.CreateBox('Box0', 3, this._scene);
-    box0.position = new BABYLON.Vector3(3, 30, 0);
+    DieFactory.createDie(shadowGenerator, this._scene);
+    DieFactory.createDie(shadowGenerator, this._scene);
+    DieFactory.createDie(shadowGenerator, this._scene);
 
-
-    box0.rotation.y = 4;
-    box0.rotation.x = 3;
-    const materialWood = new BABYLON.StandardMaterial('wood', this._scene);
-    materialWood.diffuseTexture = new BABYLON.Texture('/assets/textures/dice-1.png', this._scene);
-    materialWood.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-    box0.material = materialWood;
-
-    shadowGenerator.addShadowCaster(box0);
+    DiceTrayFactory.createDiceTray(this._scene);
 
     // Playground
     const ground = BABYLON.Mesh.CreateBox('Ground', 1, this._scene);
@@ -60,57 +44,21 @@ export class Physics {
     ground.position.y = -5.0;
     ground.checkCollisions = true;
 
-    const border0 = BABYLON.Mesh.CreateBox('border0', 1, this._scene);
-    border0.scaling = new BABYLON.Vector3(1, 100, 100);
-    border0.position.y = -5.0;
-    border0.position.x = -50.0;
-    border0.checkCollisions = true;
-
-    const border1 = BABYLON.Mesh.CreateBox('border1', 1, this._scene);
-    border1.scaling = new BABYLON.Vector3(1, 100, 100);
-    border1.position.y = -5.0;
-    border1.position.x = 50.0;
-    border1.checkCollisions = true;
-
-    const border2 = BABYLON.Mesh.CreateBox('border2', 1, this._scene);
-    border2.scaling = new BABYLON.Vector3(100, 100, 1);
-    border2.position.y = -5.0;
-    border2.position.z = 50.0;
-    border2.checkCollisions = true;
-
-    const border3 = BABYLON.Mesh.CreateBox('border3', 1, this._scene);
-    border3.scaling = new BABYLON.Vector3(100, 100, 1);
-    border3.position.y = -5.0;
-    border3.position.z = -50.0;
-    border3.checkCollisions = true;
-
     const groundMat = new BABYLON.StandardMaterial('groundMat', this._scene);
     groundMat.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
     groundMat.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
     groundMat.diffuseTexture = new BABYLON.Texture('/assets/textures/walnut-tabletop.jpg', this._scene);
     groundMat.backFaceCulling = false;
     ground.material = groundMat;
-    border0.material = groundMat;
-    border1.material = groundMat;
-    border2.material = groundMat;
-    border3.material = groundMat;
+
     ground.receiveShadows = true;
 
     // Physics
-    box0.physicsImpostor = new BABYLON.PhysicsImpostor(box0, BABYLON.PhysicsImpostor.BoxImpostor, {
-      mass: 6,
-      friction: 0.4,
-      restitution: 0.3
-    }, this._scene);
     ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, {
       mass: 0,
       friction: 0.5,
       restitution: 0.7
     }, this._scene);
-    border0.physicsImpostor = new BABYLON.PhysicsImpostor(border0, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, this._scene);
-    border1.physicsImpostor = new BABYLON.PhysicsImpostor(border1, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, this._scene);
-    border2.physicsImpostor = new BABYLON.PhysicsImpostor(border2, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, this._scene);
-    border3.physicsImpostor = new BABYLON.PhysicsImpostor(border3, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, this._scene);
   }
 
   animate(): void {
