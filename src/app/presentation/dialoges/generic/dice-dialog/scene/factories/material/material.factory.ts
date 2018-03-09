a402843e734f2c66fad57ba76f38332b02fc95e9
Meron Brouwer <meron.brouwer@gmail.com>
2018-03-09 10:38:29 +0100

    [bugfix] Fixed wrong path of textures

--- src/app/presentation/dialoges/generic/dice-dialog/scene/factories/material/material.factory.ts
+++ src/app/presentation/dialoges/generic/dice-dialog/scene/factories/material/material.factory.ts
@@ -4,10 +4,6 @@ export class MaterialFactory {
   private _dieMaterial: BABYLON.Material;
   private _wallnnutMaterial: BABYLON.Material;
 
-  private constructor() {
-
-  }
-
   public static getInstance(): MaterialFactory {
     if (!this._instance) {
       this._instance = new MaterialFactory();
@@ -30,29 +26,35 @@ export class MaterialFactory {
   }
 
   private createDieMaterial(scene: BABYLON.Scene): BABYLON.Material {
-    const textureFaceOne = new BABYLON.Texture('/assets/textures/dice-1.png', scene);
+    const textureFaceOne = new BABYLON.Texture('assets/textures/dice-1.png', scene);
     const materialFaceOne = new BABYLON.StandardMaterial('bone', scene);
     materialFaceOne.diffuseTexture = textureFaceOne;
+    materialFaceOne.freeze();
 
-    const textureFaceTwo = new BABYLON.Texture('/assets/textures/dice-2.png', scene);
+    const textureFaceTwo = new BABYLON.Texture('assets/textures/dice-2.png', scene);
     const materialFaceTwo = new BABYLON.StandardMaterial('bone', scene);
     materialFaceTwo.diffuseTexture = textureFaceTwo;
+    materialFaceTwo.freeze();
 
-    const textureFaceThree = new BABYLON.Texture('/assets/textures/dice-3.png', scene);
+    const textureFaceThree = new BABYLON.Texture('assets/textures/dice-3.png', scene);
     const materialFaceThree = new BABYLON.StandardMaterial('bone', scene);
     materialFaceThree.diffuseTexture = textureFaceThree;
+    materialFaceThree.freeze();
 
-    const textureFaceFour = new BABYLON.Texture('/assets/textures/dice-4.png', scene);
+    const textureFaceFour = new BABYLON.Texture('assets/textures/dice-4.png', scene);
     const materialFaceFour = new BABYLON.StandardMaterial('bone', scene);
     materialFaceFour.diffuseTexture = textureFaceFour;
+    materialFaceFour.freeze();
 
-    const textureFaceFive = new BABYLON.Texture('/assets/textures/dice-5.png', scene);
+    const textureFaceFive = new BABYLON.Texture('assets/textures/dice-5.png', scene);
     const materialFaceFive = new BABYLON.StandardMaterial('bone', scene);
     materialFaceFive.diffuseTexture = textureFaceFive;
+    materialFaceFive.freeze();
 
-    const textureFaceSix = new BABYLON.Texture('/assets/textures/dice-6.png', scene);
+    const textureFaceSix = new BABYLON.Texture('assets/textures/dice-6.png', scene);
     const materialFaceSix = new BABYLON.StandardMaterial('bone', scene);
     materialFaceSix.diffuseTexture = textureFaceSix;
+    materialFaceSix.freeze();
 
     const multimat = new BABYLON.MultiMaterial('multi', scene);
     multimat.subMaterials.push(materialFaceOne);
@@ -69,7 +71,8 @@ export class MaterialFactory {
     const material = new BABYLON.StandardMaterial('wallnut', scene);
     material.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
     material.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
-    material.diffuseTexture = new BABYLON.Texture('/assets/textures/walnut-tabletop.png', scene);
+    material.diffuseTexture = new BABYLON.Texture('assets/textures/walnut-tabletop.png', scene);
+    material.freeze();
 
     return material;
   }
